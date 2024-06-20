a2e4d54dc1107381e9b9231ecf027d94a9d168f8
erosalpe <eros.alpegiani@gmail.com>
2024-06-20 13:26:51 +0200

    fix fs-5 square

--- src/components/GridGenerator.vue
+++ src/components/GridGenerator.vue
@@ -126,7 +126,7 @@
         
         <div class="row justify-content-center"  v-if="generate">
             <div class="d-flex flex-wrap gap-4 pt-5 col-sm-12 col-md-11 justify-content-center">
-                <div :class="['square', {clicked: isClicked}]" v-for="i in lastNumber - firstNumber + 1" :key="i" :id="i" @click="toggleClass(i)" :style="{ borderColor: borderColor }">
+                <div :class="['square', 'fw-bold', 'fs-5', {clicked: isClicked}]" v-for="i in lastNumber - firstNumber + 1" :key="i" :id="i" @click="toggleClass(i)" :style="{ borderColor: borderColor }">
                     {{ firstNumber -1 + i }}
                 </div>
             </div>
@@ -146,7 +146,7 @@
 
     .square{
         border: 1px solid;
-        padding: 10px;
+        padding: 5px;
         display: flex;
         align-items: center;
         justify-content: center;
