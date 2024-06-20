f025c04e952e543ae25105fdd8f414a5007a7b6a
erosalpe <eros.alpegiani@gmail.com>
2024-06-20 11:15:07 +0200

    fix link

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,7 +6,7 @@
 
 
 <template>
-  <p class="text-center pt-5">Made with &hearts; by <a class="text-decoration-none text-warning" href="https://www.linkedin.com/in/eros-alpegiani/">erosalpe</a><br>https://lottery-grid-generator.vercel.app/</p>
+  <p class="text-center pt-5">Made with &hearts; by <a class="text-warning" href="https://www.linkedin.com/in/eros-alpegiani/">erosalpe</a><br>https://lottery-grid-generator.vercel.app/</p>
   <h2 class="text-center">Lottery Grid Generator</h2>
 </template>
 
